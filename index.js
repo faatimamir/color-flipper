1131608a76682d6f8c55292a819147c3c4322487
fatima <faatima.mir@gmail.com>
2022-09-15 12:44:47 +0500

    mention name of color

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ let btn1 = document.getElementById("btn")
 const colors = ["red", "blue" ,"green","orange","grey","lightblue","white" ,"purple"]
 let color = document.querySelector(".color")
 let name1 = document.getElementById("name")
+function change(){
+    let randamnum = Math.floor(Math.random() * colors.length) 
+    document.body.style.backgroundColor = colors[randamnum]
+    name1.innerHTML = colors[randamnum]
+
+}
