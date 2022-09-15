4b303cba0cd1dfb1abd8710f59710b2c22ea97ea
fatima <faatima.mir@gmail.com>
2022-09-15 12:43:09 +0500

    change color function

---
+++ index.js
@@ -0,0 +1,4 @@
+let btn1 = document.getElementById("btn")
+const colors = ["red", "blue" ,"green","orange","grey","lightblue","white" ,"purple"]
+let color = document.querySelector(".color")
+let name1 = document.getElementById("name")
